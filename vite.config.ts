fe13f05ab93c79db8e3e04874a917bdbdce6b8df
dm1sh <me@dmitriy.icu>
2021-10-20 02:46:38 +0300

    Added favicon, manifest generation and graphics for it

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      includeAssets: ["robots.txt", "favicon.ico", "favicon.svg"],
+      manifest: {
+        name: "TooDo",
+        short_name: "TooDo",
+        description: "Task management application",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
